496cd522a83ecc8dfc886833f1edbaaea0034464
Guilherme Lima <guilherme.lima@alura.com>
2022-06-07 08:32:49 -0300

    media dos alunos

--- media.cpp
+++ media.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <locale.h>
 #include <stdio.h>
-main ()
+#include <iomanip> 
+
+int main ()
 
 {	
 	setlocale(LC_ALL,"portuguese");
 	int nralunos;
-	printf("Quantos alunos você deseja lançar as notas. Máx 5 \n");
+	printf("Quantos alunos vocÃª deseja lanÃ§ar as notas. Max 5 \n");
 	scanf("%d",&nralunos);
-	system("cls");
+	system("clear");
 	
 	
 	float p1[nralunos];
@@ -16,6 +18,7 @@ main ()
 	float trab1[nralunos];
 	float trab2[nralunos];
 	float media[nralunos];
+	float mediageral;
 	float pi[nralunos];
 	char nome[nralunos][10];
 	int i;
@@ -28,47 +31,41 @@ main ()
 	{
 		
 		printf("=================\n");
-		printf("	Menu de Opções	 \n");
+		printf("	Menu de OpÃ§Ãµes	 \n");
 		printf("=================\n");
-		printf("1- Lançar alunos \n");
-		printf("2- Lançar notas \n");
+		printf("1- LanÃ§ar alunos \n");
+		printf("2- LanÃ§ar notas \n");
 		printf("3- Consultar media geral da turma \n");
 		printf("4- Consultar boletim de um aluno \n");
 		printf("5- Consultar boletim de todos os alunos  \n");
 		printf("6- sair \n");
 		printf(" escolha a opcao\n");
 		scanf("%d",&opcao);
-		system("cls");
+		system("clear");
 		
 		if (opcao==1)
 		{
-			system("cls");
+			system("clear");
 			for (i=1; i<=nralunos;i++)
 			{
 				printf("================================================================\n");
-				printf("                        Lançar alunos             \n");
+				printf("                        LanÃ§ar alunos             \n");
 				printf("================================================================\n");
 				printf("digite o nome do aluno %d \n",i);
-				scanf("%s",&nome[i]);
+				scanf("%s",nome[i]);
 				system("pause");
-				system("cls");
+				system("clear");
 			}
-			//for (i=1; i<=nralunos;i++)
-			//{
-				//printf("nome = %s \n",nome[i]);
-				
-			//}
-			
 		}
 			else
 			{
 				if(opcao==2)
 				{
-					system("cls");
+					system("clear");
 					for (i=1; i<=nralunos;i++)
 					{
 					printf("===================\n");
-					printf("     Lançar Notas	   \n");
+					printf("     Lanï¿½ar Notas	   \n");
 					printf("===================\n");
 					printf (" digite a p1 do(a) %s \n",nome[i]);
 					scanf ("%f", &p1[i]);
@@ -80,8 +77,9 @@ main ()
 					scanf("%f", &trab2[i]);
 					printf("digite a pi do(a) %s \n",nome[i]);
 					scanf("%f", &pi[i]);
-					system("cls");
+					system("clear");
 					media[i]= ((p1[i] * 0.15) + (p2[i] * 0.15) + (trab1[i] * 0.10) + (trab2[i] * 0.10) + (pi[i] * 0.5));
+					printf("%.2f", media[i]);
 					}
 				}
 			
@@ -89,25 +87,24 @@ main ()
 				{
 					if(opcao==3)
 					{
-						system("cls");
-						for (i=1; i<=nralunos;i++)
-						{
-							printf("===================\n");
-							printf("     Media geral	   \n");
-							printf("===================\n");
-							printf ("a media dos alunos(a) %s é igual a %f",nome[i],media[i]);
-							scanf("%f",&media[i]);
-							media[i]= %.2f \n ",p1[i] * 0.15,p2[i] * 0.15,trab1[i] * 0.10,trab2[i] * 0.10,pi[i] * 0.50,media[i]);
-							system("cls");
-							
-						}	
+						system("clear");
+						printf("===================\n");
+						printf("     A mÃ©dia geral dos alunos	   \n");
+						printf("===================\n");
+						for	(i=1; i<=nralunos;i++)
+								{
+									mediageral += media[i]; 
+								}
+						mediageral = mediageral / nralunos;	
+
+						printf("%.2f\n\n", mediageral);
 					}
 				
 						else
 						{
 							if(opcao==4)
 							{
-								system("cls");
+								system("clear");
 								for	(i=1; i<=nralunos;i++)
 								{
 									printf("===================\n");
@@ -119,7 +116,7 @@ main ()
 								{
 									if(opcao==5)
 									{
-										system("cls");
+										system("clear");
 										for	(i=1; i<=nralunos;i++)
 										{
 											printf("===================\n");
